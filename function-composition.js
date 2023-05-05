0c57b837e4feadc5dbcefa683885b4f9ca35eab9
FeraVerto <themightymasha@gmail.com>
2023-05-05 16:07:15 +0400

    function-composition

--- function-composition.js
+++ function-composition.js
@@ -14,5 +14,10 @@ let compose = function(functions) {
     
 };
 
+//Еще один вариант решения
+// var compose = function(functions) {
+//     return x => functions.reduceRight((acc,f)=>f(acc),x)
+// };
+
 const fn = compose([x => x + 1, x => x * x, x => 2 * x])
 fn(4)
