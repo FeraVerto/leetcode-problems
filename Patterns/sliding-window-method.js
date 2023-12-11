b3702d7ba455991b181601b9585cfa696d784cd6
FeraVerto <themightymasha@gmail.com>
2023-12-11 22:16:59 +0400

    substrings-of-size-three-with-distinct-characters

--- Patterns/sliding-window-method.js
+++ Patterns/sliding-window-method.js
@@ -66,3 +66,52 @@ let findMaxAverage = function(nums, k) {
 };
 
 findMaxAverage([1,12,-5,-6,50,3], 4);  //12.750000
+
+//https://leetcode.com/problems/substrings-of-size-three-with-distinct-characters/description/
+
+let countGoodSubstrings = function(s) {
+    let count = 0;
+    
+    let startIndex = 0;
+    let endIndex = 2;
+
+    
+    while(endIndex <= s.length - 1) {
+        let subString = s.slice(startIndex, endIndex + 1);
+        let set = new Set(subString);
+        if(set.size === subString.length) {
+            count++;
+        }
+        startIndex++;
+        endIndex++;
+    }
+
+    return count;
+};
+
+countGoodSubstrings("aababcabc");
+
+// Input: s = "aababcabc"
+// Output: 4
+// Explanation: There are 7 substrings of size 3: "aab", "aba", "bab", "abc", "bca", "cab", and "abc".
+// The good substrings are "abc", "bca", "cab", and "abc".
+
+//https://leetcode.com/problems/fruit-into-baskets/
+
+// Задача: У вас есть массив чисел. Каждое число представляет "тип". Ваша задача — найти
+//  самый длинный подмассив, в котором содержатся не более двух уникальных типов чисел.
+
+// Пример:
+
+// Допустим, у вас есть массив [1, 2, 1, 2, 3].
+// В этом массиве подмассив [1, 2, 1, 2] содержит только два типа чисел (1 и 2) и является
+//  самым длинным таким подмассивом. Подмассив [2, 1, 2, 3] уже содержит три разных типа 
+//  чисел, что больше допустимого.
+
+let totalFruit = function(fruits) {   
+
+};
+
+totalFruit([1,1,2,2,3]); //4
+totalFruit([1,2,3,2,2]); //4
+totalFruit([0,1,2,2]); //3
