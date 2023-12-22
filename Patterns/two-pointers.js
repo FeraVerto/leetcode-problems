ccc989601eec8c76d5bc0b6ddcfdff5aeef1e21e
FeraVerto <themightymasha@gmail.com>
2023-12-22 13:38:41 +0400

    number-of-arithmetic-triplets

--- Patterns/two-pointers.js
+++ Patterns/two-pointers.js
@@ -91,3 +91,80 @@ let countPairs2 = function(nums, target) {
 
 countPairs2([-1,1,2,3,1], 2); //3
 countPairs2([-6,2,5,-2,-7,-1,3], -2); //10
+
+
+//https://leetcode.com/problems/number-of-arithmetic-triplets/
+
+// You are given a 0-indexed, strictly increasing integer array nums 
+// and a positive integer diff. A triplet (i, j, k) is an arithmetic 
+// triplet if the following conditions are met:
+
+// i < j < k,
+// nums[j] - nums[i] == diff, and
+// nums[k] - nums[j] == diff
+
+// (1, 2, 4) - 1, 4, 7 is an arithmetic triplet because both 7 - 4 == 3 and 4 - 1 == 3.
+// (2, 4, 5) - 4, 7, 10 is an arithmetic triplet because both 10 - 7 == 3 and 7 - 4 == 3.
+
+let arithmeticTriplets = function(nums, diff) {
+    let leftPoint = 0;
+    let rightPoint = 1;
+
+    let count = 0;
+
+    while(rightPoint !== nums.length) {
+        if(nums[rightPoint] - nums[leftPoint] === 2 * diff) {
+            let res = nums[rightPoint] - diff;
+            let leftIndex = leftPoint;
+            let rightIndex = rightPoint;
+            let currentCount = 0;
+            
+            while(leftIndex < rightIndex) {
+                let midIndex = Math.floor((leftIndex + rightIndex) / 2);
+
+                if(nums[midIndex] === res) {
+                    currentCount++;
+                }
+
+                if(nums[midIndex] < res) {
+                    leftIndex = midIndex + 1;
+                } else {
+                    rightIndex = midIndex;
+                }
+            }
+            count += currentCount;
+            leftPoint++;
+        }
+        
+        if(nums[rightPoint] - nums[leftPoint] < 2 * diff) {
+            rightPoint++;
+        } else {
+            leftPoint++;
+        }
+    }
+
+    return count;
+};
+
+// сначала ищем разницу в 2diff, а потом в diff
+// (1, 2, 4) - 1, 4, 7 is an arithmetic triplet because both 7 - 4 == 3 and 4 - 1 == 3.
+// (2, 4, 5) - 4, 7, 10 is an arithmetic triplet because both 10 - 7 == 3 and 7 - 4 == 3.
+
+// |   |
+// 1   2   3   4   5   6   7
+//2 - 1 = 1
+// |       |
+// 1   2   3   4   5   6   7
+//3 - 1 = 2
+// |           |
+// 1   2   3   4   5   6   7
+// ищем в оставшемся массиве 4 + 3 бинарным поиском
+//     |       |
+// 1   2   3   4   5   6   7
+// 4 - 2 = 2
+//     |           |
+// 1   2   3   4   5   6   7
+//5 - 2 = 3
+// ищем в оставшемся массиве 5 + 3 бинарным поиском
+arithmeticTriplets([4,5,6,7,8,9], 2); //2
+arithmeticTriplets([0,1,4,6,7,10], 3); //2
